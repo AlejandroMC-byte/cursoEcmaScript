3f3f96925211950e479b2ddf14ed0d86551c6bab
AlejandroMC-byte <alejandro.montero@correounivalle.edu.co>
2022-07-23 11:02:34 -0500

    ecmaScript10 y ecmaScript11 con imports dinamicos

--- src/es10/index.js
+++ src/es10/index.js
@@ -4,4 +4,26 @@ console.log(array.flat(2));
 
 let array2 = [1,2,3,4,5];
 
-console.log(array.flatMap(value =>[value,value*2]))
+console.log(array2.flatMap(value =>[value,value*2]));
+
+let hello = '     hello world';
+console.log(hello);
+console.log(hello.trimStart());
+
+let hello2 ='Hello world     ';
+console.log(hello);
+console.log(hello.trimEnd());
+
+try{
+
+}catch{
+    error //disponible sin necesidad de declararlo en el catch
+}
+
+let entries =[['name','oscar'],['age',32]];
+
+console.log(Object.fromEntries(entries));
+
+let mySimbol = `My Symbol`;
+let symbol =Symbol(mySimbol);
+console.log(symbol.description);
