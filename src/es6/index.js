570c6d7ee6a272b96450c39fb2216db2dcb43f61
AlejandroMC-byte <alejandro.montero@correounivalle.edu.co>
2022-07-23 01:21:40 -0500

    Clases, modulos y funciones generadoras

--- src/es6/index.js
+++ src/es6/index.js
@@ -39,8 +39,8 @@ let person = {
 console.log(person.name,person.age)
 
 //Ecmascript6
-let {name,age,country} =person;
-console.log(name,age,country);
+let {age,country} =person;
+console.log(age,country);
 
 let team1 =['Oscar', 'Julian', 'Ricardo'];
 let team2 =['Valeria', 'Yesica', 'Camila'];
@@ -61,3 +61,85 @@ console.log(globalVar)
 }
 //CONST para una declaracion que es constante y jamas cambia
 const a ='b';
+
+
+//Asignacion de objetos
+
+let name ='Oscar';
+let ages =32;
+
+//esc5
+obj ={name:name,age:age};
+//esc6
+obj2 = {name,ages};
+console.log(obj2);
+
+//Función tradicional
+function nombre (parámetros) {
+    return (valorRetornado)
+}
+
+//Función flecha
+const nombre = (parámetros) => {
+    return (valorRetornado)
+}
+
+//Función tradicional
+function suma (num1, num2) {
+    return num1 + num2
+}
+
+//Función flecha
+const suma = (num1, num2) => num1 + num2
+
+//Promesas
+const helloPromise = () => {
+    return new Promise((resolve,reject)=>{
+        if(true){
+            resolve('Hey!')
+        } else{
+            reject('Ups!!')
+        }
+    })
+}
+helloPromise()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+//CLASES
+
+class calculator{
+    constructor(){
+        this.valueA=0;
+        this.valueB=0;
+    }
+    sum(valuaA,valueB){
+        this.valueA=valuaA;
+        this.valueB=valuaB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+//Importacion y Exportacion de funciones
+
+import {hello} from './module'
+
+hello();
+
+//Funcion generadora
+function* helloWorld(){
+    if(true){
+        yield 'Hello, ';
+    }
+    if(true){
+        yield 'World';
+    }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
