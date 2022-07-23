6d8b255e05bb790515132394d034af492ccd2bf8
AlejandroMC-byte <alejandro.montero@correounivalle.edu.co>
2022-07-23 01:29:09 -0500

    EcmaScript7 (includes y operaciones exponenciales)

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+/*
+    funcion INCLUDES que me permite ver si existe un string o elemento
+    dentro de un arreglo
+*/
+let numbers =[1,2,3,7,8];
+
+if(numbers.includes(7)){
+    console.log('Si se encuentra el valor 7');
+} else{
+    console.log('No se encuentra');
+}
+/*
+    Para generar operaciones exponenciales utilizamos 'base' ** 'Exponente'
+*/
+let base =4;
+let exponente =3;
+let result =base ** exponente;
+
+console.log(result);
